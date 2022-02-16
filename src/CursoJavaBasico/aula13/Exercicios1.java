258b53527825e21857bf20564deb144f19283058
Dianine <dianine23@gmail.com>
2022-02-16 10:23:19 -0300

    Operadores-Aritmeticos-Logicos-Relacionais

---
+++ src/CursoJavaBasico/aula13/Exercicios1.java
@@ -0,0 +1,12 @@
+package CursoJavaBasico.aula13;
+
+public class Exercicios1 {
+
+	public static void main(String[] args) {
+		System.out.println("Ola Mundo!!");
+		
+		
+
+	}
+
+}
---
+++ src/CursoJavaBasico/aula13/OperadoresRelacionais.java
@@ -0,0 +1,21 @@
+package CursoJavaBasico.aula13;
+
+public class OperadoresRelacionais {
+
+	public static void main(String[] args) {
+		int x1 = 1;
+		int x2 = 2;
+		
+		
+		System.out.println(" x1 == x2: " + (x1 == x2));
+		System.out.println(" x1 != x2: " + (x1 != x2));
+		System.out.println(" x1 > x2: " + (x1 > x2));
+		System.out.println(" x1 >= x2: " + (x1 >= x2));
+		System.out.println(" x1 < x2: " + (x1 < x2));
+		System.out.println(" x1 <= x2: " + (x1 <= x2));
+		
+		
+
+	}
+
+}
