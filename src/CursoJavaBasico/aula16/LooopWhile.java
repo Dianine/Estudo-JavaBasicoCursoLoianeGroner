958599c8782dc929ff27d9b2f01e37dc28f9ca17
Dianine <dianine23@gmail.com>
2022-02-17 20:26:11 -0300

    Video Aula com exemplos

---
+++ src/CursoJavaBasico/aula16/LooopWhile.java
@@ -0,0 +1,27 @@
+package CursoJavaBasico.aula16;
+
+import java.util.Scanner;
+
+public class LooopWhile {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Entre com um Numero: ");
+		int n = sc.nextInt();
+		
+		int i = 1;
+		
+		while(i <= n) {
+			System.out.println("Imprima: " + i);
+			i++;
+		}
+		System.out.println(i); 
+		do {
+			i++;
+			System.out.println("Valor de i: " + i);
+		}while (i < 13);
+		System.out.println(i);
+	}
+
+}
